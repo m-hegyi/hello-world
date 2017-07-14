d3b197db2a47aa4925fb4b3ab611d7ac5359bdc1
Mario Hegyi <mario.hegyi93@gmail.com>
2017-07-14 12:17:34 +0200

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -1,19 +1,3 @@
 <?php
 
-class HelloWorld {
-
-    public $myValue;
-
-    public function my_hello_value($value) {
-        $this->myValue = $value;
-    }
-
-    public function my_hello_function($param) {
-        echo $param . $this->myValue . "<br>";
-    }
-}
-
-$text = new hello_world();
-
-$text->my_hello_value("world");
-$text->my_hello_function("Hello, ");
+echo ('Hello World');

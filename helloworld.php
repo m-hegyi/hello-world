1aa1f72218bfa3cd08a2b92b2ca8679e5aedce34
Mario Hegyi <mario.hegyi93@gmail.com>
2017-07-18 20:41:25 +0200

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -1,1 +1,37 @@
 <?php
+
+class HelloWorld 
+{
+	protected $hello;
+	protected $world;
+
+	public function __toString()
+	{
+		return $this->hello . " " .$this->world . "!";
+	}
+
+	public function __set ($name, $value)
+	{
+
+		switch ($name) {
+
+			case 'hello':
+				$this->hello = $value;
+				break;
+
+			case 'world':
+				$this->world = $value;
+				break;
+
+			default:
+				echo $name . " Not found";
+		}
+	}
+}
+
+$hello_world = new HelloWorld();
+
+$hello_world->hello = 'Hello';
+$hello_world->world = 'World';
+
+echo $hello_world;
